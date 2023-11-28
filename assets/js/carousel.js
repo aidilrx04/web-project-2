b4ffb831d14c30b5d372f32f32e974375f1daa2b
aidilrx04 <mrcrayv14@gmail.com>
2023-11-28 09:09:03 +0800

    feat: reinitize slick on page change

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,10 +1,34 @@
 $(document).ready(function () {
-  $("#main-carousel").slick({
-    easing: "ease-in-out",
-    prevArrow: `<button class="prev"><i class="ph ph-caret-left"></i></button>`,
-    nextArrow: `<button class="next"><i class="ph ph-caret-right"></i></button>`,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    pauseOnHover: true,
+  let pages = $('[data-role="page"]');
+
+  console.log(pages);
+
+  $(document).on("pagechange", function () {
+    console.log("page changed :(");
+    initializeSlick();
   });
+
+  $(`[data-role="page"]`).on("pagechange", function () {
+    console.log("pagechange");
+  });
+
+  initializeSlick();
+  function initializeSlick() {
+    $(document).ready(function () {
+      console.log("initializing slick");
+      try {
+        $(".carousel").slick({
+          easing: "ease-in-out",
+          prevArrow: `<button class="prev"><i class="ph ph-caret-left"></i></button>`,
+          nextArrow: `<button class="next"><i class="ph ph-caret-right"></i></button>`,
+          autoplay: true,
+          autoplaySpeed: 5000,
+          pauseOnHover: true,
+        });
+      } catch (e) {
+        console.error("Error while creating slick");
+        console.error(e);
+      }
+    });
+  }
 });
